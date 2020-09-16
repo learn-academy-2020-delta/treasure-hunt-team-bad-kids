5047d0784df8da398e176bc74b4da74a8eb959a3
LEARN Academy <devjoelmichael@protonmail.com>
2020-09-16 14:39:39 -0700

    working on question marks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react'
 import Square from './components/Square'
+import tree from './assets/tree-solid.svg'
+import question from './assets/question-solid.svg'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squaresArray: [...Array(9).fill("?")]
+      squaresArray: [...Array(9).fill("?")],
+      question: "question-solid.svg",
+      tree: "tree-solid.svg"
     }
   }
 
-  Alert = () => {
-
-  }
 
   render(){
     let { squaresArray } = this.state
     let boxes = squaresArray.map((box, index) => {
       return (
+        <>
         <Square
           index = { index }
           key = { index }
+          tree = { tree }
+          question = { question }
         />
+        </>
       )
     })
     return(
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 
 class Square extends Component{
   render(){
+    let { index } = this.props
+    let { tree } = this.props
+    let { question} = this.props
     return(
       <React.Fragment>
-        <div className= "square" onClick= >
-          { this.props.index }
+        <div className= "square" onClick={ () => { <img src = { question }/> }}>
+          <img src = { tree } />
         </div>
       </React.Fragment>
     )
