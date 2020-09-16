4679f0128c15b841e5fc8cfd0a8d4bd94e8f6405
brianthomashammond <brianthomashammond@gmail.com>
2020-09-16 11:42:34 -0700

    treasure hunt start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      squaresArray: [...Array(9).fill("?")]
     }
   }
 
+  Alert = () => {
+
+  }
+
   render(){
+    let { squaresArray } = this.state
+    let boxes = squaresArray.map((box, index) => {
+      return (
+        <Square
+          index = { index }
+          key = { index }
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <div className= "gameBoard">
+          { boxes }
+        </div>
       </React.Fragment>
     )
   }
